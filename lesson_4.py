e98ed1f0a287af98f867328ae072a099f3bc0609
Igor <igorg789170@gmail.com>
2026-06-05 18:57:36 +0600

    Абстракция

---
+++ lesson_4.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+# абстрактный класс
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+    @abstractmethod
+    def test(self):
+        pass
+
+# конкретные классы
+class Dog(Animal):
+    def make_sound(self):
+        print("гав-гав")
+
+    def test(self):
+        print("test in dog")
+
+class Cat(Animal):
+    def make_sound(self):
+        print("мяу")
+
+puppy = Dog()
+puppy.make_sound()
