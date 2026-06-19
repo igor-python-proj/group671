b4126769d7c8eadf3139c78dc7bcb5b4adba2270
Igor <igorg789170@gmail.com>
2026-06-19 19:00:38 +0600

    Получение данных, изменение, удаление(CRUD)

--- lessons/database.py
+++ lessons/database.py
@@ -1,11 +1,18 @@
 def create_tables(conn):
     conn.execute("""
+    DROP TABLE IF EXISTS students
+    """)
+    conn.execute("""
     CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         age INTEGER,
         city TEXT
     )
     """)
+
+# CRUD - create, read, update, delete
+# добавление, получение, изменение, удаление
 
 def add_student(
         conn,
@@ -14,8 +21,69 @@ def add_student(
         city
 ):
     conn.execute("""
-    INSERT INTO students VALUES
+    INSERT INTO students(name, age,city) VALUES
     (?, ?, ?)
     """,
         (name, age, city))
+    conn.commit()
+
+def get_students(conn):
+    result = conn.execute("""
+        SELECT age, name FROM students
+    """)
+    # return result.fetchone()
+    # return result.fetchmany(2)
+    return result.fetchall()
+
+def get_student_by_name(
+        conn,
+        name
+):
+    result = conn.execute("""
+        SELECT * FROM students
+        WHERE name = ? AND age < 30
+        """,
+        (name,)
+    )
+    return result.fetchall()
+
+
+def get_student_by_id(
+        conn,
+        student_id
+):
+    result = conn.execute("""
+        SELECT * FROM students
+        WHERE id = ?
+        """,
+        (student_id,)
+    )
+    return result.fetchone()
+
+def delete_student(
+        conn,
+        student_id
+):
+    conn.execute("""
+    DELETE FROM students
+    WHERE id = ?
+    """, (student_id,)
+    )
+    conn.commit()
+
+def update_student(
+        conn,
+        name,
+        age,
+        city,
+        student_id
+):
+    conn.execute("""
+    UPDATE students
+    SET name = ?, 
+    age = ?, city = ?
+    WHERE id = ?
+    """,
+    (name, age, city, student_id)
+    )
     conn.commit()
--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sqlite3
 
 from lessons.database import (
     create_tables,
-    add_student
+    add_student,
+    get_students,
+    get_student_by_name,
+    get_student_by_id,
+    delete_student,
+    update_student,
 )
-
 
 if __name__ == "__main__":
     connection = sqlite3.connect(
@@ -18,5 +22,64 @@ if __name__ == "__main__":
         19,
         'Karakol'
     )
+    add_student(
+        connection,
+        'Aijamal',
+        23,
+        'Naryn'
+    )
+    add_student(
+        connection,
+        'Igor',
+        25,
+        'Karakol'
+    )
+    add_student(
+        connection,
+        'Igor',
+        32,
+        'Bishkek'
+    )
+    print("== get students ==")
+    students = get_students(connection)
+    for st in students:
+        print(st)
+
+    print("== search by name ==")
+    student = get_student_by_name(
+        connection,
+        'Igor',
+    )
+    print(student)
+    student2 = get_student_by_name(
+        connection,
+        'Dastan',
+    )
+    print(student2)
+    student3 = get_student_by_id(
+        connection,
+        10,
+    )
+    print(student3)
+
+    print("== delete students ==")
+    delete_student(
+        connection,
+        4
+    )
+    print(get_students(connection))
+
+    print("== update students ==")
+    update_student(
+        connection,
+        'Artur',
+        20,
+        'Kara-Balta',
+        1
+    )
+    print(get_student_by_id(
+        connection,
+        1
+    ))
 
     connection.close()
